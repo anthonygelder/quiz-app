31a67241596a9da171df1ea25bda8d863dd2c3e1
Anthony Gelder <anthony.gelder@gmail.com>
2019-07-19 22:18:40 -0700

    js file and data store added

---
+++ app.js
@@ -0,0 +1,104 @@
+const STORE = [
+    {
+    question: "Who is the franchise leader in rushing yards?",
+    answer: [ 
+        'Marshawn Lynch',
+        'Shaun Alexander',
+        'Curt Warner',
+        'Chris Warren'
+    ],
+    correctAnswer: "Shawn Alexander"
+    },
+    {
+    question: "Who is the franchise leader in touchdown passes?",
+    answer: [
+        'Russell Wilson',
+        'Matt Hasselbeck',
+        'Dave Krieg',
+        'Charlie Whitehurst'
+    ],
+    correctAnswer: 'Russell Wilson'
+    },
+    {
+    question: "What year did the franchise begin?",
+    answer: [
+        '1970',
+        '2012',
+        '1085',
+        '1976'
+    ],
+    correctAnswer: '1976'
+    },
+    {
+    question: 'What conference did the team originally start in?',
+    answer: [
+        'AFC',
+        'NFC',
+        'XFL',
+        'CFL'
+    ],
+    correctAnswer: 'NFC'
+    },
+    {
+    question: "How many Super Bowls have the Seahawks been to?",
+    answer: [
+        '1',
+        '3',
+        '5',
+        '10'
+    ],
+    correctAnswer: '3'
+    },
+    {
+    question: 'Who is the franchise leader in interceptions?',
+    answer: [
+        'Dave Brown',
+        'Richard Sherman',
+        'Eugene Robinson',
+        'Earl Thomas'
+    ],
+    correctAnswer: 'Dave Brown'
+    },
+    {
+    question: 'What season did the Seahawks win the Superbowl?',
+    answer: [
+        '05-06',
+        '13-14',
+        '14-15',
+        '83-84'
+    ],
+    correctAnswer: '13-14'
+    },
+    {
+    question: 'Who is the franchise leader in recieving yards?',
+    answer: [
+        'Doug Baldwin',
+        'Steve Largent',
+        'Brian Blades',
+        'Joey Galloway'
+    ],
+    correctAnswer: 'Steve Largent'
+    },
+    {
+    question: 'Who is the franchise leader in passing yards?',
+    answer: [
+        'Russell Wilson',
+        'Dave Krieg',
+        'Matt Hasselbeck',
+        'Jim Zorn'
+    ],
+    correctAnswer: 'Matt Hasselbeck'
+    },
+    {
+    question: 'Who is the winningest coach in franchise history?',
+    answer: [
+        'Mike Holmgren',
+        'Chuck Knox',
+        'Pete Carroll',
+        'Jack Patera'
+    ],
+    correctAnswer: 'Pete Carroll'
+    }
+]
+
+
